22d40a5299f9e4a18a161c68d436dc9823a0def9
Jonathan Widén <jonathan.loke.widen@gmail.com>
2023-01-22 18:20:05 +0100

    GH-5: Run clippy --fix

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -7,33 +7,44 @@ use std::collections::HashMap;
 
 pub fn eval(document: &Element) -> String {
     // Note that the real program of course won't be hard-coded to transpile into html like this
-    return elem_to_html(&document);
+    elem_to_html(document)
 }
 
 // Example function turning an element to html
 fn elem_to_html(elem: &Element) -> String {
     match elem {
         Element::Data(str) => str.to_string(),
-        Element::Node { name, attributes, children } => {
-            inode_to_html(&name, &attributes, &children)
-        }
+        Element::Node {
+            name,
+            attributes,
+            children,
+        } => inode_to_html(name, attributes, children),
     }
 }
 
 // Example turning inode (non-leaf node) to html, to make above function cleaner
-fn inode_to_html(name: &str, attributes: &HashMap<String, String>, children: &Vec<Element>) -> String {
+fn inode_to_html(
+    name: &str,
+    _attributes: &HashMap<String, String>,
+    children: &[Element],
+) -> String {
     match name {
-        "Document" => format!("<main>\n{}\n</main>",
-                              children.iter()
-                                  .map(|e| elem_to_html(&e))
-                                  .collect::<Vec<String>>()
-                                  .join("\n")
+        "Document" => format!(
+            "<main>\n{}\n</main>",
+            children
+                .iter()
+                .map(elem_to_html)
+                .collect::<Vec<String>>()
+                .join("\n")
         ),
-        "Paragraph" => format!("<p>\n{}\n</p>",
-                               children.iter()
-                                   .map(|e| elem_to_html(&e))
-                                   .collect::<Vec<String>>()
-                                   .join("\n")),
-        &_ => panic!("No inode=>html for node '{name}'")
+        "Paragraph" => format!(
+            "<p>\n{}\n</p>",
+            children
+                .iter()
+                .map(elem_to_html)
+                .collect::<Vec<String>>()
+                .join("\n")
+        ),
+        &_ => panic!("No inode=>html for node '{name}'"),
     }
 }
