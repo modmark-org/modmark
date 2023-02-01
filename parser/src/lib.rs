4e43836bde83a85c72b4ced45c550b13a9fa2bff
Jonathan Widén <jonathan.loke.widen@gmail.com>
2023-02-01 22:22:43 +0100

    GH-12: Add ability to parse multiline modules, paragraphs and documents

--- parser/src/lib.rs
+++ parser/src/lib.rs
@@ -3,30 +3,23 @@
 
 extern crate core;
 
-use std::any::Any;
 use std::collections::HashMap;
-use std::fmt::{Display, Formatter};
+
 use std::mem;
 
-use lazy_static::lazy_static;
-use nom::bytes::complete::{
-    is_a, is_not, take_till, take_till1, take_until, take_until1, take_while, take_while1,
-};
+use nom::bytes::complete::{take_till, take_until, take_until1, take_while1};
 use nom::character::complete::{
-    alphanumeric1, anychar, char, multispace0, multispace1, newline, none_of, not_line_ending,
-    space0, space1,
+    alphanumeric1, char, line_ending, multispace0, multispace1, none_of, space0, space1,
 };
-use nom::character::{is_alphanumeric, is_space};
+
 use nom::error::Error;
-use nom::multi::{fold_many0, fold_many1, many0, many1, separated_list0, separated_list1};
+use nom::multi::{fold_many1, many1, separated_list0, separated_list1};
 use nom::sequence::{delimited, pair, preceded, separated_pair, terminated};
 use nom::{
-    branch::*,
-    bytes::complete::{tag, take_while_m_n},
-    combinator::*,
-    sequence::tuple,
-    Finish, IResult, Parser,
+    branch::*, bytes::complete::tag, combinator::*, FindSubstring, IResult, InputTake, Parser,
 };
+
+use Element::Node;
 
 use crate::Element::{Data, ModuleInvocation};
 
@@ -59,15 +52,7 @@ fn get_arg_separator_parser<'a>(inline: bool) -> impl Parser<&'a str, (), Error<
     map(pair(opt(char(',')), space), |_| ())
 }
 
-/*fn parse_inline_arg_separator(input: &str) -> IResult<&str, ()> {
-    map(pair(opt(char(',')), space1), |_| ())(input)
-}
-
-fn parse_multiline_arg_separator(input: &str) -> IResult<&str, ()> {
-    map(pair(opt(char(',')), multispace1), |_| ())(input)
-}*/
-
-fn parse_arg_value(input: &str) -> IResult<&str, &str> {
+fn arg_value_parser(input: &str) -> IResult<&str, &str> {
     alt((
         delimited(char('"'), take_until1(r#"""#), char('"')),
         take_while1(|c: char| c.is_ascii_alphanumeric()),
@@ -83,18 +68,20 @@ fn named_arg(input: &str) -> IResult<&str, (String, String)> {
     separated_pair(
         map(many1(alt((alphanumeric1, tag("_")))), |cs| cs.join("")),
         get_kv_separator_parser(true),
-        map(parse_arg_value, |x| x.to_string()),
+        map(arg_value_parser, |x| x.to_string()),
     )(input)
 }
 
 fn unnamed_arg<'a>(inline: bool) -> impl Parser<&'a str, String, Error<&'a str>> {
     map(
-        terminated(parse_arg_value, peek(not(get_kv_separator_parser(inline)))),
+        terminated(arg_value_parser, peek(not(get_kv_separator_parser(inline)))),
         |s| s.to_string(),
     )
 }
 
-fn get_module_args_parser<'a>(inline: bool) -> impl Parser<&'a str, ModuleArguments, Error<&'a str>> {
+fn get_module_args_parser<'a>(
+    inline: bool,
+) -> impl Parser<&'a str, ModuleArguments, Error<&'a str>> {
     map(
         opt(alt((
             map(
@@ -131,21 +118,24 @@ fn parse_module_name(input: &str) -> IResult<&str, &str> {
     take_while1(|c: char| c == '-' || c == '_' || c.is_ascii_alphanumeric())(input)
 }
 
-fn parse_inline_module_invocation(input: &str) -> IResult<&str, (String, ModuleArguments)> {
+fn get_module_invocation_parser<'a>(
+    inline: bool,
+) -> impl Parser<&'a str, (String, ModuleArguments), Error<&'a str>> {
     map(
         delimited(
             char('['),
             pair(
                 parse_module_name,
-                opt(preceded(
-                    get_arg_separator_parser(true),
-                    get_module_args_parser(true),
+                opt(delimited(
+                    get_arg_separator_parser(inline),
+                    get_module_args_parser(inline),
+                    opt(get_arg_separator_parser(inline)),
                 )),
             ),
             char(']'),
         ),
         |(name, args)| (name.to_string(), args.unwrap_or_default()),
-    )(input)
+    )
 }
 
 fn parse_opening_delim(input: &str) -> IResult<&str, Option<&str>> {
@@ -154,13 +144,37 @@ fn parse_opening_delim(input: &str) -> IResult<&str, Option<&str>> {
     }))(input)
 }
 
-fn take_body_helper<'a>(
+fn get_multiline_body_parser<'a>(
     delim: Option<&'_ str>,
-) -> impl Fn(&'a str) -> IResult<&'a str, &'a str> + '_ {
+) -> impl Parser<&'a str, &'a str, Error<&'a str>> + '_ {
     move |i: &'a str| {
         if let Some(opening_delim) = delim {
             let closing = closing_delim(opening_delim);
-            let res = terminated(take_until(closing.as_str()), tag(closing.as_str()))(i);
+            let res = delimited(
+                line_ending,
+                take_until(closing.as_str()),
+                tag(closing.as_str()),
+            )(i);
+            res
+        } else {
+            preceded(
+                line_ending,
+                take_until1("\r\n\r\n").or(take_until1("\n\n").or(rest)),
+            )(i)
+        }
+    }
+}
+
+fn get_inline_body_parser<'a>(
+    delim: Option<&'_ str>,
+) -> impl Parser<&'a str, &'a str, Error<&'a str>> + '_ {
+    move |i: &'a str| {
+        if let Some(opening_delim) = delim {
+            let closing = closing_delim(opening_delim);
+            let res = terminated(
+                take_until_no_newlines(closing.as_str()),
+                tag(closing.as_str()),
+            )(i);
             res
         } else {
             preceded(space0, take_till(|c: char| c.is_ascii_whitespace()))(i)
@@ -168,13 +182,48 @@ fn take_body_helper<'a>(
     }
 }
 
+/*fn until_tag_without_line_endings<'a>(
+    tag: &'a str,
+) -> impl Parser<&'a str, &'a str, Error<&'a str>> + '_ {
+    move |i: &'a str| {
+        let t = <&str>::clone(&tag);
+        let until_res = take_until(t)(i);
+        if let Ok((_, taken)) = until_res {
+            let nl_pos = i.find('\n');
+            if nl_pos.map_or(true, |i| i > taken.len()) {
+                until_res
+            } else {
+                fail(i)
+            }
+        } else {
+            until_res
+        }
+    }
+}*/
+
+// this will use take_until to take a substring until a given tag, but won't take any newlines.
+// if a newline occurs before the tag, this will fail
+// don't mention the body, it is copied from the definition of take_until
+fn take_until_no_newlines(tag: &str) -> impl Fn(&str) -> IResult<&str, &str, Error<&str>> + '_ {
+    move |i: &str| match i.find_substring(tag) {
+        None => fail(i),
+        Some(index) => {
+            if i.find('\n').map_or(true, |i| i > index) {
+                Ok(i.take_split(index))
+            } else {
+                fail(i)
+            }
+        }
+    }
+}
+
 fn parse_inline_module_body(input: &str) -> IResult<&str, &str> {
-    flat_map(parse_opening_delim, take_body_helper)(input)
+    flat_map(parse_opening_delim, get_inline_body_parser)(input)
 }
 
 fn parse_inline_module(input: &str) -> IResult<&str, Element> {
     map(
-        pair(parse_inline_module_invocation, parse_inline_module_body),
+        pair(get_module_invocation_parser(true), parse_inline_module_body),
         |((name, args), body)| ModuleInvocation {
             name,
             args,
@@ -184,21 +233,43 @@ fn parse_inline_module(input: &str) -> IResult<&str, Element> {
     )(input)
 }
 
+fn parse_multiline_module_body(input: &str) -> IResult<&str, &str> {
+    flat_map(parse_opening_delim, get_multiline_body_parser)(input)
+}
+
+fn parse_multiline_module(input: &str) -> IResult<&str, Element> {
+    map(
+        pair(
+            get_module_invocation_parser(false),
+            parse_multiline_module_body,
+        ),
+        |((name, args), body)| ModuleInvocation {
+            name,
+            args,
+            body: body.to_string(),
+            one_line: false,
+        },
+    )(input)
+}
+
 fn escape(char: char) -> char {
     char
 }
 
-fn parse_line(input: &str) -> IResult<&str, Vec<Element>> {
+fn parse_paragraph_elements(input: &str) -> IResult<&str, Vec<Element>> {
     map(
         fold_many1(
-            or::or4(
+            or::or5(
                 parse_inline_module,
-                preceded(char('\\'), newline),
+                preceded(char('\\'), line_ending),
                 preceded(char('\\'), none_of("\r\n")),
                 none_of("\r\n"),
+                // note: do NOT use not_line_ending, it matches successfully on empty string
+                // so that would break this
+                terminated(line_ending, peek(none_of("\r\n")))
             ),
             || (Vec::new(), String::new()),
-            |(acc_vec, acc_str), (opt_inline, opt_esc_newline, opt_esc_char, opt_char)| {
+            |(acc_vec, acc_str), (opt_inline, _opt_esc_line_ending, opt_esc_char, opt_char, opt_line_ending)| {
                 let mut elems = acc_vec;
                 let mut string = acc_str;
 
@@ -211,6 +282,8 @@ fn parse_line(input: &str) -> IResult<&str, Vec<Element>> {
                     string.push(escape(esc_char));
                 } else if let Some(n_char) = opt_char {
                     string.push(n_char);
+                } else if let Some(line_ending) = opt_line_ending {
+                    string.push_str(line_ending);
                 }
                 (elems, string)
             },
@@ -225,8 +298,32 @@ fn parse_line(input: &str) -> IResult<&str, Vec<Element>> {
     )(input)
 }
 
+fn parse_paragraph(input: &str) -> IResult<&str, Element> {
+    map(parse_paragraph_elements, |elems| Node {
+        name: "Paragraph".to_string(),
+        attributes: Default::default(),
+        children: elems,
+    })(input)
+}
+
+fn parse_multiple_paragraphs(input: &str) -> IResult<&str, Vec<Element>> {
+    separated_list0(
+        preceded(line_ending, many1(line_ending)),
+        parse_multiline_module.or(parse_paragraph),
+    )(input)
+    //many0(parse_paragraph)(input)
+}
+
+fn parse_document(input: &str) -> IResult<&str, Element> {
+    map(parse_multiple_paragraphs, |paras| Node {
+        name: "Document".to_string(),
+        attributes: Default::default(),
+        children: paras,
+    })(input)
+}
+
 fn do_parse(input: &str) -> Element {
-    let res = parse_line(input);
+    let res = parse_document(input);
     match &res {
         Ok(_) => {}
         Err(x) => {
@@ -235,12 +332,13 @@ fn do_parse(input: &str) -> Element {
     }
 
     println!("{:?}", res.as_ref().unwrap());
-    let actual_res = res.map(|(_, elems)| Element::Node {
+    /*let actual_res = res.map(|(_, elems)| Element::Node {
         name: "Document".to_string(),
         attributes: HashMap::new(),
         children: elems,
     });
-    return actual_res.unwrap();
+    return actual_res.unwrap();*/
+    res.unwrap().1
 }
 
 fn closing_delim(string: &str) -> String {
@@ -272,8 +370,10 @@ fn pretty_rows(element: &Element, include_attributes: bool) -> Vec<String> {
     let mut strs = vec![];
 
     match element {
-        Element::Data(str) => strs.push(format!(r#""{str}""#)),
-        Element::Node {
+        Data(str) => str.lines().enumerate().for_each(|(idx, line)| {
+            strs.push(format!("{} {line}", if idx == 0 { '>' } else { '|' }))
+        }),
+        Node {
             name,
             attributes,
             children,
@@ -318,10 +418,6 @@ fn pretty_rows(element: &Element, include_attributes: bool) -> Vec<String> {
             body,
             one_line,
         } => {
-            //format like:
-            //name(args) {
-            //  body
-            //}
             let args = {
                 let p1 = &args.positioned;
                 let p2 = args.named.as_ref().map(|args| {
--- parser/src/or.rs
+++ parser/src/or.rs
@@ -1,5 +1,6 @@
 use nom::Parser;
 
+#[allow(dead_code)]
 pub fn or_other<I: Clone, O1, O2, E, F, G>(
     mut p1: F,
     mut p2: G,
@@ -10,7 +11,7 @@ where
     G: Parser<I, O2, E>,
 {
     move |i: I| match p1.parse(i.clone()) {
-        Err(nom::Err::Error(e1)) => match p2.parse(i.clone()) {
+        Err(nom::Err::Error(e1)) => match p2.parse(i) {
             Err(nom::Err::Error(e2)) => Err(nom::Err::Error(e1.or(e2))),
             res => res.map(|(a, r2)| (a, (None, Some(r2)))),
         },
@@ -18,6 +19,7 @@ where
     }
 }
 
+#[allow(dead_code)]
 pub fn or2<I: Clone, O1, O2, E, F, G>(p1: F, p2: G) -> impl Parser<I, (Option<O1>, Option<O2>), E>
 where
     E: nom::error::ParseError<I>,
@@ -27,6 +29,7 @@ where
     or_other(p1, p2)
 }
 
+#[allow(dead_code)]
 pub fn or3<I: Clone, O1, O2, O3, E, F, G, H>(
     p1: F,
     p2: G,
@@ -41,6 +44,7 @@ where
     or_other(p1, or_other(p2, p3)).map(flatten)
 }
 
+#[allow(dead_code)]
 pub fn or4<I: Clone, O1, O2, O3, O4, E, F, G, H, J>(
     p1: F,
     p2: G,
@@ -62,6 +66,7 @@ where
         })
 }
 
+#[allow(dead_code)]
 pub fn or5<I: Clone, O1, O2, O3, O4, O5, E, F, G, H, J, K>(
     p1: F,
     p2: G,
@@ -89,9 +94,115 @@ where
         })
 }
 
-fn flatten<A, B, C>(
-    (fst, snd): (Option<A>, Option<(Option<B>, Option<C>)>),
-) -> (Option<A>, Option<B>, Option<C>) {
+#[allow(dead_code)]
+pub fn or6<I: Clone, O1, O2, O3, O4, O5, O6, E, F, G, H, J, K, L>(
+    p1: F,
+    p2: G,
+    p3: H,
+    p4: J,
+    p5: K,
+    p6: L,
+) -> impl Parser<
+    I,
+    (
+        Option<O1>,
+        Option<O2>,
+        Option<O3>,
+        Option<O4>,
+        Option<O5>,
+        Option<O6>,
+    ),
+    E,
+>
+where
+    E: nom::error::ParseError<I>,
+    F: Parser<I, O1, E>,
+    G: Parser<I, O2, E>,
+    H: Parser<I, O3, E>,
+    J: Parser<I, O4, E>,
+    K: Parser<I, O5, E>,
+    L: Parser<I, O6, E>,
+{
+    or_other(
+        p1,
+        or_other(p2, or_other(p3, or_other(p4, or_other(p5, p6)))),
+    )
+    .map(flatten)
+    .map(|(a, b, c)| {
+        let (b, c, d) = flatten((b, c));
+        (a, b, c, d)
+    })
+    .map(|(a, b, c, d)| {
+        let (c, d, e) = flatten((c, d));
+        (a, b, c, d, e)
+    })
+    .map(|(a, b, c, d, e)| {
+        let (d, e, f) = flatten((d, e));
+        (a, b, c, d, e, f)
+    })
+}
+
+#[allow(dead_code)]
+pub fn or7<I: Clone, O1, O2, O3, O4, O5, O6, O7, E, F, G, H, J, K, L, M>(
+    p1: F,
+    p2: G,
+    p3: H,
+    p4: J,
+    p5: K,
+    p6: L,
+    p7: M,
+) -> impl Parser<
+    I,
+    (
+        Option<O1>,
+        Option<O2>,
+        Option<O3>,
+        Option<O4>,
+        Option<O5>,
+        Option<O6>,
+        Option<O7>,
+    ),
+    E,
+>
+where
+    E: nom::error::ParseError<I>,
+    F: Parser<I, O1, E>,
+    G: Parser<I, O2, E>,
+    H: Parser<I, O3, E>,
+    J: Parser<I, O4, E>,
+    K: Parser<I, O5, E>,
+    L: Parser<I, O6, E>,
+    M: Parser<I, O7, E>,
+{
+    or_other(
+        p1,
+        or_other(
+            p2,
+            or_other(p3, or_other(p4, or_other(p5, or_other(p6, p7)))),
+        ),
+    )
+    .map(flatten)
+    .map(|(a, b, c)| {
+        let (b, c, d) = flatten((b, c));
+        (a, b, c, d)
+    })
+    .map(|(a, b, c, d)| {
+        let (c, d, e) = flatten((c, d));
+        (a, b, c, d, e)
+    })
+    .map(|(a, b, c, d, e)| {
+        let (d, e, f) = flatten((d, e));
+        (a, b, c, d, e, f)
+    })
+    .map(|(a, b, c, d, e, f)| {
+        let (e, f, g) = flatten((e, f));
+        (a, b, c, d, e, f, g)
+    })
+}
+
+type Optt<A, B> = Option<(Option<A>, Option<B>)>;
+
+fn flatten<A, B, C>((fst, snd): (Option<A>, Optt<B, C>)) -> (Option<A>, Option<B>, Option<C>) {
     if let Some((fst_2, snd_2)) = snd {
         (fst, fst_2, snd_2)
     } else {
