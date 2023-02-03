489adbd967fb02b15fc95458d8ba9e7077281e3f
Eli Adelhult <eli.adelhult@outlook.com>
2023-02-03 21:11:09 +0100

    GH-14: Add module info test + remove bold module

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -12,3 +12,52 @@ pub use loaded_module::{Arg, LoadedModule, ModuleInfo, NodeName, Transform};
 pub fn eval(_document: &Element, _ctx: &mut Context) -> String {
     "TODO".to_string()
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn module_info() {
+        let ctx = Context::default();
+        let info = ctx.get_module_info("Module test").unwrap().clone();
+
+        let foo = ModuleInfo {
+            name: "Module test".to_string(),
+            version: "1".to_string(),
+            transforms: vec![
+                Transform {
+                    from: "[table]".to_string(),
+                    to: "table".to_string(),
+                    arguments: vec![Arg {
+                        name: "border".to_string(),
+                        default: Some("black".to_string()),
+                        description: "What color the border should be".to_string(),
+                    }],
+                },
+                Transform {
+                    from: "table".to_string(),
+                    to: "html".to_string(),
+                    arguments: vec![],
+                },
+                Transform {
+                    from: "table".to_string(),
+                    to: "latex".to_string(),
+                    arguments: vec![],
+                },
+                Transform {
+                    from: "row".to_string(),
+                    to: "html".to_string(),
+                    arguments: vec![],
+                },
+                Transform {
+                    from: "row".to_string(),
+                    to: "latex".to_string(),
+                    arguments: vec![],
+                },
+            ],
+        };
+
+        assert_eq!(info, foo);
+    }
+}
