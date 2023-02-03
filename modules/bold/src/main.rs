567dbe42585d2f7d66d39d8e386dbf8b0cc3daff
Eli Adelhult <eli.adelhult@outlook.com>
2023-02-03 17:45:14 +0100

    GH-14: Fix transforms func in bold module

--- modules/bold/src/main.rs
+++ modules/bold/src/main.rs
@@ -1,36 +1,17 @@
-use std::env;
-use std::io;
-
 fn main() {
     println!("Hello, world!");
 }
 
 #[no_mangle]
 pub fn name() {
-    println!("MyCoolBoldModule 😎")
+    println!("Bold")
 }
 
 #[no_mangle]
 pub fn version() {
-    println!("0.1.1");
+    println!("0.0.1");
 }
 
-#[no_mangle]
-pub fn test_envs() {
-    let args: Vec<String> = env::args().collect();
-
-    println!("{}", args[1]);
-}
-
-#[no_mangle]
-pub fn transforms() {
-    println!("bold->html");
-    println!("bold->latex");
-}
-
-#[no_mangle]
-pub fn attributes() {
-    println!("attribute-five");
-    println!("list-symbol=bullet");
-    println!("bullet-spacing=1");
+fn transforms() {
+    println!("bold -> html");
 }
