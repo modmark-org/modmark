63278ca377fb4d3f8d6664ab969424b0159f55e7
Eli Adelhult <eli.adelhult@outlook.com>
2023-02-03 17:33:59 +0100

    GH-14: Support multiple transforms in a module

--- modules/test-module/src/main.rs
+++ modules/test-module/src/main.rs
@@ -12,16 +12,37 @@ pub fn version() {
 
 #[no_mangle]
 pub fn transforms() {
-    println!("[verbatim indent=4] -> html latex");
+    /*
+    [verbatim] -> tex html
+    foo       - An example of a required positional argument
+    ident = 4 - The number of spaces to indent
+
+    [table] -> table
+
+    table -> html latex
+
+    row - html latex
+    */
+
+    println!("[verbatim] -> html latex");
+    println!("foo - An example of a required postional argument");
+    println!("ident = 4 - The number of spaces to indent the verbatim block");
+    println!("");
+
     println!("[table] -> table");
+    println!("");
+
     println!("table -> html latex");
+    println!("");
+
     println!("row -> html latex");
+    println!("");
 }
 
 fn main() {
     let args: Vec<String> = env::args().skip(1).collect();
     let node_name = &args[0];
-    
+
     let mut body = String::new();
     io::stdin().read_line(&mut body).unwrap();
 
@@ -33,4 +54,3 @@ fn main() {
         other => eprintln!("This node does not support node conversions from {other}"),
     }
 }
-
