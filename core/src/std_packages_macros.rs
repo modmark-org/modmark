332f8ae8be6107768e2522e469b80d8d9ecaf1fa
Jonathan Widén <jonathan.loke.widen@gmail.com>
2023-02-21 16:29:30 +0100

    GH-82: Comment code and general clean-up

--- core/src/std_packages_macros.rs
+++ core/src/std_packages_macros.rs
@@ -1,3 +1,27 @@
+/// This macro defines takes a declaration of native packages, and generates two functions,
+/// `pub fn native_package_list() -> Vec<PackageInfo>` returning a list of PackageInfos for the
+/// packages, and `pub fn handle_native(...) -> Result<Either<Element, String>, CoreError>` which
+/// takes a call to a native module and calls it by the handle given in the declaration.
+///
+/// The native module handlers should have the signature:
+/// `pub fn fn_name(ctx: &mut Context, body: &str, args: HashMap<String, String>,
+///     inline: bool, output_format: &OutputFormat) -> Result<Either<Element, String>, CoreError>;`
+///
+/// Example usage:
+/// ```rust,ignore
+/// define_native_packages! {
+///     "package_name_1" => {
+///         "module_name_1", vec![ //a vec containing the `ArgInfo`s
+///             ArgInfo {
+///                 name: "key".to_string(),
+///                 default: None,
+///                 description: "The key to set".to_string()
+///             }
+///         ] => handle_module_1,
+///         "module_name_2", vec![] => handle_module_2
+///     }
+/// }
+/// ```
 macro_rules! define_native_packages {
     ($($name:expr => { $($transform:expr, $arg_info:expr => $handler:ident),* $(,)? };)*) => {
         pub fn native_package_list() -> Vec<PackageInfo> {
@@ -57,6 +81,9 @@ macro_rules! define_native_packages {
     }
 }
 
+/// This macro takes a list of standard package names and includes them from the
+/// `OUT_DIR/pkg_name/wasm32-wasi/release/pkg_name.wasm` wasm file. It is important
+/// that the standard package cargo name is the same as the containing folder name.
 macro_rules! define_standard_package_loader {
     ($($name:expr),* $(,)?) => {
         pub fn load_standard_packages(ctx: &mut Context) -> Result<(), CoreError>{
