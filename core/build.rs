b37d4a764b37f50f39dd2dd210dcfb0e4bbfafae
Jonathan Widén <jonathan.loke.widen@gmail.com>
2023-02-27 17:21:06 +0100

    GH-78: Rename modules into packages

--- core/build.rs
+++ core/build.rs
@@ -11,7 +11,7 @@ fn main() {
     build_packages()
 }
 
-// This build script will build all of the crates found in the top level "modules" directory
+// This build script will build all of the crates found in the top level "packages" directory
 // into wasm binary files which allows core to load them from the path
 // env!("OUT_DIR")/out/<name_of_module>/wasm32_wasi/release/<name_of_module>.wasm
 fn build_packages() {
@@ -23,34 +23,34 @@ fn build_packages() {
         path
     };
 
-    let modules_path = workspace_path.join("modules");
+    let packages_path = workspace_path.join("packages");
 
-    println!("cargo:rerun-if-changed={}", modules_path.to_string_lossy());
+    println!("cargo:rerun-if-changed={}", packages_path.to_string_lossy());
 
-    let modules_dir = fs::read_dir(&modules_path).expect("No modules directory found.");
+    let packages_dir = fs::read_dir(&packages_path).expect("No packages directory found.");
 
-    // Build the wasm file for every crate in the modules directory.
-    modules_dir
+    // Build the wasm file for every crate in the packages directory.
+    packages_dir
         .into_iter()
         .map(|f| f.unwrap())
         .filter(|f| f.file_type().unwrap().is_dir())
         .map(|module| {
-            build_wasm_module(
+            build_wasm_package(
                 &module.file_name().to_string_lossy(),
-                &modules_path,
+                &packages_path,
                 &out_path,
             )
         })
         .for_each(|(mut f, name)| {
             let exit = f.wait().expect("failed to launch wasm build");
             if !exit.success() {
-                println!("cargo:warning=failed to build module: {name}")
+                println!("cargo:warning=failed to build package: {name}")
             }
         });
 }
 
-fn build_wasm_module(name: &str, modules_path: &Path, output_path: &Path) -> (Child, String) {
-    let manifest_path = modules_path.join(name).join("Cargo.toml");
+fn build_wasm_package(name: &str, packages_path: &Path, output_path: &Path) -> (Child, String) {
+    let manifest_path = packages_path.join(name).join("Cargo.toml");
     let output_sub_dir = output_path.join(name);
 
     let child = Command::new(env!("CARGO"))
