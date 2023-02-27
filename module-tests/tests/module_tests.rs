a9c504148e23a0becf66985335ee9e4f6eedb2ea
Jonathan Widén <jonathan.loke.widen@gmail.com>
2023-02-27 17:02:24 +0100

    GH-101: Use datatest_stable

--- module-tests/tests/module_tests.rs
+++ module-tests/tests/module_tests.rs
@@ -1,4 +1,13 @@
-#[test]
-fn test() {
-    assert_eq!(1,1);
+use std::path::Path;
+
+fn test_module_input(file: &Path) -> datatest_stable::Result<()> {
+    // path is module-name/tests/file-name.json
+
+    Ok(())
 }
+
+datatest_stable::harness!(
+    test_module_input,
+    "../modules",
+    r"[^/\\]*[/\\]tests[/\\].*\.json$"
+);
