8436e86d0a20ca24957687f1a4a59ea495e58fc3
bruhn <bruhng@student.chalmers.se>
2023-03-01 12:58:27 +0100

    GH-58: Fix desync with main

--- packages/code/src/main.rs
+++ packages/code/src/main.rs
@@ -3,16 +3,10 @@ use std::fmt::Write;
 use std::io::{self, Read};
 
 use serde_json::{json, Value};
-<<<<<<< HEAD
 use syntect::easy::HighlightLines;
 use syntect::parsing::SyntaxSet;
 use syntect::highlighting::{ThemeSet, Color};
 use syntect::html::{styled_line_to_highlighted_html, IncludeBackground};
-=======
-use syntect::parsing::SyntaxSet;
-use syntect::highlighting::{Color, ThemeSet};
-use syntect::html::highlighted_html_for_string;
->>>>>>> ae9220c (GH-60: Add minimal version of code package)
 
 fn main() {
     let args: Vec<String> = env::args().skip(1).collect();
@@ -29,11 +23,7 @@ fn main() {
 fn manifest() {
     print!("{}", serde_json::to_string(&json!(
         {
-<<<<<<< HEAD
         "name": "code",
-=======
-        "name": "Standard code package",
->>>>>>> ae9220c (GH-60: Add minimal version of code package)
         "version": "0.1",
         "description": "This package provides syntax highlighting in [code] modules",
         "transforms": [
@@ -41,7 +31,6 @@ fn manifest() {
                 "from": "code",
                 "to": ["html"],
                 "arguments": [
-<<<<<<< HEAD
                     {"name": "lang", "default": "txt", "description":
                         "The language to be highlighted. For available languages, see \
                         https://github.com/sublimehq/Packages"},
@@ -51,10 +40,6 @@ fn manifest() {
                         "Theme of the code section. For available themes, see \
                         https://docs.rs/syntect/latest/syntect/highlighting/struct.ThemeSet.html#method.load_defaults"},
                     {"name": "bg", "default": "default", "description": "Background of the code section"},
-=======
-                    {"name": "lang", "description": "The language to be highlighted"},
-                    {"name": "fontsize", "default": "12", "description": "The size of the font"}
->>>>>>> ae9220c (GH-60: Add minimal version of code package)
                 ],
             }
         ]
@@ -63,25 +48,15 @@ fn manifest() {
     .unwrap());
 }
 
-<<<<<<< HEAD
 fn transform(from: &str, to: &str) {
     match from {
         "code" => transform_code(to),
         other => {
             eprintln!("Package does not support {other}");
-=======
-fn transform(from: &String, to: &String) {
-    match from.as_str() {
-        "code" => transform_code(to),
-        other => {
-            eprintln!("Package does not support {other}");
-            return;
->>>>>>> ae9220c (GH-60: Add minimal version of code package)
         }
     }
 }
 
-<<<<<<< HEAD
 fn transform_code(to: &str) {
     macro_rules! get_arg {
         ($input:expr, $arg:expr) => {
@@ -93,10 +68,6 @@ fn transform_code(to: &str) {
         }
     }
     match to {
-=======
-fn transform_code(to: &String) {
-    match to.as_str() {
->>>>>>> ae9220c (GH-60: Add minimal version of code package)
         "html" => {
             let input: Value = {
                 let mut buffer = String::new();
@@ -104,7 +75,6 @@ fn transform_code(to: &String) {
                 serde_json::from_str(&buffer).unwrap()
             };
 
-<<<<<<< HEAD
             let code = input["data"].as_str().unwrap();
             let lang = get_arg!(input, "lang");
             let font_size = get_arg!(input, "font_size");
@@ -178,43 +148,3 @@ fn get_highlighted(code: &str, lang: &String, tm: &str) -> (String, Option<Color
         (html.join("<br>"), Some(Color{r: 200, g: 200, b: 200, a: 255}))
     }
 }
-=======
-            let Value::String(lang) = &input["arguments"]["lang"] else {
-                panic!("No lang argument was provided");
-            };
-            let Value::String(size) = &input["arguments"]["fontsize"] else {
-                panic!("No fontsize argument was provided");
-            };
-
-            let code = input["data"].as_str().unwrap();
-
-            let style = format!("style=\\\"font-size:{size}px\\\"");
-            let ss = SyntaxSet::load_defaults_newlines();
-            let ts = ThemeSet::load_defaults();
-            let syntax = ss.find_syntax_by_extension(lang).unwrap();
-            let theme = &ts.themes["base16-ocean.light"];
-
-            let html = highlighted_html_for_string(code, &ss, syntax, theme).unwrap();
-            let formatted = html
-                .replace("\"", "\\\"")
-                .lines()
-                .filter(|s| !s.is_empty())
-                .collect::<Vec<&str>>()
-                .join("<br>");
-
-            let mut output = String::new();
-            output.push('[');
-            write!(output, r#"{{"name": "raw", "data": "<code {style}>"}},"#).expect("");
-            write!(output, r#"{{"name": "raw", "data": "{formatted}"}},"#).expect("");
-            output.push_str(r#"{"name": "raw", "data": "</code>"}"#);
-            output.push(']');
-
-            print!("{output}");
-        }
-        other => {
-            eprintln!("Cannot convert code to {other}");
-            return;
-        }
-    }
-}
->>>>>>> ae9220c (GH-60: Add minimal version of code package)
