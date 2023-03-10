234ca3fbf3c6260fa1c57e506354f6449406712a
Eli Adelhult <eli.adelhult@outlook.com>
2023-03-10 16:05:53 +0100

    GH-45: Run compiler in web worker

---
+++ playground/static/compiler.js
@@ -0,0 +1,40 @@
+// FIXME: Replace with a normal ES module import once Firefox adds support for js modules in web workers
+importScripts("./pkg/web_bindings.js");
+
+let loaded = false;
+wasm_bindgen("./pkg/web_bindings_bg.wasm").then(() => {
+    loaded = true;
+    postMessage({ type: "init" });
+});
+
+onmessage = (event) => {
+    if (!loaded) return;
+
+    try {
+        let result;
+        switch (event.data.type) {
+            case "ast":
+                result = wasm_bindgen.ast(event.data.source);
+                break;
+            case "ast_debug":
+                result = wasm_bindgen.ast_debug(event.data.source);
+                break;
+            case "json_output":
+                result = wasm_bindgen.json_output(event.data.source);
+                break;
+            case "package_info":
+                result = wasm_bindgen.package_info(event.data.source);
+                break;
+            case "transpile":
+                result = wasm_bindgen.transpile(event.data.source, event.data.format);
+                break;
+
+            case "package_info":
+                result = wasm_bindgen.package_info();
+        }
+        postMessage({ result: result, success: true, ...event.data });
+    } catch (error) {
+        console.log(error);
+        postMessage({ error: error, success: false, ...event.data })
+    }
+};
