60efb27e9729ae666b9923efeb0d2a30a1bacbfd
Hugo Mårdbrink <hugo@mardbrink.se>
2023-04-11 15:27:29 +0200

    GH-192: Add internal referencing

--- packages/link/src/main.rs
+++ packages/link/src/main.rs
@@ -2,7 +2,6 @@ use std::env;
 use std::io::{self, Read};
 
 use serde_json::{json, Value};
-
 fn main() {
     let args: Vec<String> = env::args().skip(1).collect();
     let action = &args[0];
@@ -21,14 +20,26 @@ fn manifest() {
         serde_json::to_string(&json!(
             {
             "name": "link",
-            "version": "0.2",
-            "description": "This package supports [link] modules",
+            "version": "0.3",
+            "description": "This package supports [link] modules. It can link internally with labels and references, or externally with URLs.",
             "transforms": [
                 {
                     "from": "link",
                     "to": ["html", "latex"],
                     "arguments": [
                         {"name": "label", "default": "", "description": "Label for link"}
+                    ],
+                },
+                {
+                    "from": "label",
+                    "to": ["html", "latex"],
+                    "arguments": [],
+                },
+                {
+                    "from": "reference",
+                    "to": ["html", "latex"],
+                    "arguments": [
+                        {"name": "display", "default": "", "description": "Displayed label for reference (Only HTML)"}
                     ],
                 }
             ]
@@ -41,6 +52,8 @@ fn manifest() {
 fn transform(from: &str, to: &str) {
     match from {
         "link" => transform_link(to),
+        "label" => transform_label(to),
+        "reference" => transform_reference(to),
         other => {
             eprintln!("Package does not support {other}");
         }
@@ -48,14 +61,14 @@ fn transform(from: &str, to: &str) {
 }
 
 fn transform_link(to: &str) {
+    let input: Value = {
+        let mut buffer = String::new();
+        io::stdin().read_to_string(&mut buffer).unwrap();
+        serde_json::from_str(&buffer).unwrap()
+    };
+
     match to {
         "html" => {
-            let input: Value = {
-                let mut buffer = String::new();
-                io::stdin().read_to_string(&mut buffer).unwrap();
-                serde_json::from_str(&buffer).unwrap()
-            };
-
             let label = input["arguments"]
                 .get("label")
                 .map(|val| val.as_str().unwrap())
@@ -75,12 +88,6 @@ fn transform_link(to: &str) {
             print!("{output}");
         }
         "latex" => {
-            let input: Value = {
-                let mut buffer = String::new();
-                io::stdin().read_to_string(&mut buffer).unwrap();
-                serde_json::from_str(&buffer).unwrap()
-            };
-
             let label = input["arguments"]
                 .get("label")
                 .map(|val| val.as_str().unwrap())
@@ -97,6 +104,91 @@ fn transform_link(to: &str) {
         }
         other => {
             eprintln!("Cannot convert link to {other}");
+        }
+    }
+}
+
+fn transform_label(to: &str) {
+    let input: Value = {
+        let mut buffer = String::new();
+        io::stdin().read_to_string(&mut buffer).unwrap();
+        serde_json::from_str(&buffer).unwrap()
+    };
+
+    match to {
+        "html" => {
+            let label = input["data"].as_str().unwrap();
+            let escaped_label = label.replace('"', "%22");
+
+            let label_tag = format!(r#"<span id="{escaped_label}">"#);
+
+            let output = json!([
+                {"name": "raw", "data": label_tag},
+                {"name": "raw", "data": "</span>"},
+            ]);
+
+            print!("{output}");
+        }
+        "latex" => {
+            let label = input["data"].as_str().unwrap();
+            let escaped_label = label.replace('"', "%22");
+
+            let label_tag = format!(r#"\label{{{}}}"#, escaped_label);
+
+            let output = json!([
+                {"name": "raw", "data": label_tag},
+            ]);
+
+            print!("{output}");
+        }
+        other => {
+            eprintln!("Cannot convert label to {other}");
+        }
+    }
+}
+
+fn transform_reference(to: &str) {
+    let input: Value = {
+        let mut buffer = String::new();
+        io::stdin().read_to_string(&mut buffer).unwrap();
+        serde_json::from_str(&buffer).unwrap()
+    };
+
+    match to {
+        "html" => {
+            let label = input["data"].as_str().unwrap();
+            let mut escaped_label = label.replace('"', "%22");
+            escaped_label.insert(0, '#');
+
+            let display = input["arguments"]
+                .get("display")
+                .map(|val| val.as_str().unwrap())
+                .unwrap_or_else(|| "");
+
+            let label_tag = format!(r#"<a href="{escaped_label}">"#);
+
+            let output = json!([
+                {"name": "raw", "data": label_tag},
+                {"name": "raw", "data": display},
+                {"name": "raw", "data":  "</a>"},
+            ]);
+
+            print!("{output}");
+        }
+        "latex" => {
+            let label = input["data"].as_str().unwrap();
+            let escaped_label = label.replace('"', "%22");
+
+            let label_tag = format!(r#"\ref{{{}}}"#, escaped_label);
+
+            let output = json!([
+                {"name": "raw", "data": label_tag},
+            ]);
+
+            print!("{output}");
+        }
+        other => {
+            eprintln!("Cannot convert ref to {other}");
         }
     }
 }
