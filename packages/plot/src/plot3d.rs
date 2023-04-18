06f87c0e3c1a88c57453436ff9a94f716c798c52
Forsinge <forsinge@chalmers.se>
2023-04-18 13:27:47 +0200

    GH-237: Add styling arguments, improve plots, allow arbitrary variables

---
+++ packages/plot/src/plot3d.rs
@@ -0,0 +1,12 @@
+/*
+
+use serde_json::{json, Value};
+use std::io::Write;
+use std::fs::File;
+use plotters::prelude::*;
+use plotters::style::ShapeStyle;
+use base64::{engine::general_purpose, Engine as _};
+
+use plot::{raw, get_calc_funcs, verify_function, eval_function};
+
+ */
